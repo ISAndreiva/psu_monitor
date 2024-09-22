703105c759352853ca99b0f487a6eba21dc06df6
ISAndreiva <43719525+ISAndreiva@users.noreply.github.com>
2024-09-22 15:53:09 +0300

    Implement auto-updating data

--- psu/urls.py
+++ psu/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
+    path('get_psu/<int:pk>', views.get_psu, name="get_psu"),
 ]
--- psu/views.py
+++ psu/views.py
@@ -2,8 +2,9 @@
 # Create your views here.
 from django.views import generic
 from django.shortcuts import get_object_or_404, render
+from django.http import JsonResponse
 from .models import PSU_serial, PSU_base
-
+import datetime
 
 class IndexView(generic.ListView):
     template_name = 'psu/index.html'
@@ -12,3 +13,21 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return PSU_base.objects.all()
 
+
+def get_psu(request, pk):
+    psu = get_object_or_404(PSU_base, pk=pk)
+    return JsonResponse({
+        'last_updated': int(psu.last_updated.timestamp()),
+        'name': psu.name,
+        'voltageIN': psu.get_voltageIN(),
+        'currentIN': psu.get_currentIN(),
+        'powerIN': psu.get_powerIN(),
+        'voltageOUT': psu.get_voltageOUT(),
+        'currentOUT': psu.get_currentOUT(),
+        'powerOUT': psu.get_powerOUT(),
+        'efficiency': psu.get_efficiency(),
+        'temperature1': psu.get_temperature1(),
+        'temperature2': psu.get_temperature2(),
+        'temperature3': psu.get_temperature3(),
+        'fan_speed': psu.get_fan_speed()
+    })
